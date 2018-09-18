af760f5d55fe298d221e5d0ebf54fddf08a3918e
aki017 <aki@aki017.info>
2018-09-18 14:41:45 +0900

    sample push

--- scripts/pr-if-changed.sh
+++ scripts/pr-if-changed.sh
@@ -10,6 +10,8 @@ tar -C "$HOME" -zxf "hub-linux-amd64-$HUB.tgz"
 export PATH="$PATH:$HOME/hub-linux-amd64-$HUB/bin"
 
 key=`date "+%Y%m%d%H%M"`
+git config --global user.name  "aki017"
+git config --global user.email "aki017@users.noreply.github.com"
 git remote set-url origin https://aki017:$GITHUB_TOKEN@github.com/aki017/unity-xref
 git checkout -b pr
 git add .
