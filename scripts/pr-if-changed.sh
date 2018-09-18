16b2b47111df7490d7d4ec9e17c6bc4564cbe198
aki017 <aki@aki017.info>
2018-09-18 14:01:57 +0900

    test update

---
+++ scripts/pr-if-changed.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+if [ `git status --porcelain | wc -l` -eq 0 ]; then
+    exit 0
+fi
+
+HUB=2.5.1
+curl -LO "https://github.com/github/hub/releases/download/v$HUB/hub-linux-amd64-$HUB.tgz"
+tar -C "$HOME" -zxf "hub-linux-amd64-$HUB.tgz"
+export PATH="$PATH:$HOME/hub-linux-amd64-$HUB/bin"
+
+key=`date "+%Y%m%d%H%M"`
+git remote set-url origin https://aki017:$GITHUB_TOKEN@github.com/aki017/unity-xref
+git checkout -b pr
+git add .
+git commit -m "Auto update doc"
+git push origin pr:pr-$key
+hub pull-request -m "Auto update doc $UNITY_VERSION $key" -h pr-$UNITY_VERSION-$key
